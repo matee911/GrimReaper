7117406261b63cc15b69d33cd3c35ed279633039
matee <matee@matee.net>
2015-09-12 22:57:09 +0200

    Pre rc1 version

--- main.go
+++ main.go
@@ -1,26 +1,16 @@
 package main
 
-/*
-
-TODO:
- - rewrite to use channels
- - parametrized sock path, sigterm-delay, sigkill-delay
- - debug mode
-
-*/
-
 import (
-	"io"
-	"log"
 	"flag"
+	"io"
+	"io/ioutil"
+	"log"
 	"net"
-	"fmt"
 	"os"
 	"os/signal"
 	"strconv"
 	"strings"
 	"sync"
-	"io/ioutil"
 	"syscall"
 	"time"
 )
@@ -31,33 +21,30 @@ type Victims struct {
 }
 
 var (
-	debug bool
+	debug      bool
 	socketPath string
-	logPath string
-	stdout bool
-	logFile *os.File
-	victims = Victims{procs: make(map[int]int64)}
+	logPath    string
+	stdout     bool
+	logFile    *os.File
+	victims    = Victims{procs: make(map[int]int64)}
 
-	Debug *log.Logger
-	Info *log.Logger
-	Error *log.Logger
+	Debug    *log.Logger
+	Info     *log.Logger
+	Warning  *log.Logger
+	Error    *log.Logger
 	Critical *log.Logger
 )
-
 
 func init() {
 	flag.BoolVar(&debug, "debug", false, "Debug mode.")
 	flag.StringVar(&socketPath, "socket", "/tmp/GrimReaper.socket", "Path to the Unix Domain Socket.")
 	flag.StringVar(&logPath, "logpath", "/var/log/GrimReaper.log", "Path to the log file.")
-	flag.BoolVar(&stdout, "stdout", false, "Log to stdout/stderr instead to the log file.")
+	flag.BoolVar(&stdout, "stdout", false, "Log to stdout/stderr instead of to the log file.")
 }
 
-
 func setupLoggers(logFile *os.File) {
-	format := log.Ldate|log.Ltime|log.Lshortfile
+	format := log.Ldate | log.Ltime | log.Lshortfile
 	debugWriter := ioutil.Discard
-
-	fmt.Printf("logFile: %v", logFile)
 
 	if stdout {
 		if debug {
@@ -65,6 +52,7 @@ func setupLoggers(logFile *os.File) {
 		}
 		Debug = log.New(debugWriter, "DEBUG: ", format)
 		Info = log.New(os.Stdout, "INFO: ", format)
+		Warning = log.New(os.Stdout, "WARNING: ", format)
 		Error = log.New(os.Stderr, "Error: ", format)
 		Critical = log.New(os.Stderr, "CRITICAL: ", format)
 	} else {
@@ -73,15 +61,10 @@ func setupLoggers(logFile *os.File) {
 		}
 		Debug = log.New(debugWriter, "DEBUG: ", format)
 		Info = log.New(logFile, "INFO: ", format)
+		Warning = log.New(os.Stdout, "WARNING: ", format)
 		Error = log.New(logFile, "ERROR: ", format)
 		Critical = log.New(logFile, "CRITICAL: ", format)
-		Critical.Println("Test")
-		logFile.Sync()
 	}
-
-	d2 := []byte{115, 111, 109, 101, 10}
-	logFile.Write(d2)
-	logFile.Sync()
 }
 
 func removeOldSocket(socket string) {
@@ -93,7 +76,6 @@ func removeOldSocket(socket string) {
 	}
 }
 
-
 func main() {
 	flag.Parse()
 	var err error
@@ -101,13 +83,11 @@ func main() {
 	if !stdout {
 		logFile, err = os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 		if err != nil {
-			log.Fatalf("Cannot open log file(%v): %v", logPath, err)
+			Critical.Fatalf("Unnable to open the log file(%v): %v", logPath, err)
 		}
 		defer logFile.Close()
 	}
-	fmt.Println(logFile)
 	setupLoggers(logFile)
-
 
 	removeOldSocket(socketPath)
 
@@ -116,13 +96,12 @@ func main() {
 		panic(err)
 	}
 
-	defer log.Println("Shutting down...")
+	defer Info.Println("Shutting down...")
 	defer listener.Close()
 	defer os.Remove(socketPath)
 
 	go reaper(victims)
 	Info.Printf("Ready to accept connections (%s).\n", socketPath)
-	logFile.Sync()
 	go acceptConnections(listener, victims)
 
 	// Handle SIGINT and SIGTERM.
@@ -131,15 +110,12 @@ func main() {
 	log.Println(<-ch)
 }
 
-
 func acceptConnections(listener net.Listener, victims Victims) {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			Critical.Fatalf("Get client connection error: %v\n", err)
+			Critical.Fatalf("Got the connection's error: %v\n", err)
 		}
-
-		Debug.Printf("loc %v\n", conn.LocalAddr())
 		go handleConnection(conn)
 	}
 }
@@ -150,7 +126,7 @@ func reaper(victims Victims) {
 
 		for pid, deadline := range victims.procs {
 			if now > deadline {
-				Info.Printf("Terminating PID %d\n", pid)
+				Warning.Printf("Terminating PID %d\n", pid)
 				unregisterProcess(pid)
 				go syscall.Kill(pid, syscall.SIGTERM)
 			}
@@ -160,8 +136,7 @@ func reaper(victims Victims) {
 	}
 }
 
-
-func registerProcess(pid int, timestamp int64, timeout int64){
+func registerProcess(pid int, timestamp int64, timeout int64) {
 	victims.Lock()
 	victims.procs[pid] = timestamp + timeout
 	victims.Unlock()
@@ -177,14 +152,12 @@ func unregisterProcess(pid int) {
 func handleConnection(conn net.Conn) {
 	for {
 
-		// len("open:65000:86400") => 16
-		// "close:65000"
 		buf := make([]byte, 32)
 		nr, err := conn.Read(buf)
 		if err == io.EOF {
 			return
 		} else if err != nil {
-			Error.Printf("Get client data error: ", err)
+			Error.Printf("Got data error: ", err)
 		}
 
 		raw := string(buf[0:nr])
@@ -197,16 +170,16 @@ func handleConnection(conn net.Conn) {
 		state := data[0]
 		pid, err := strconv.ParseInt(data[1], 10, 32)
 		if err != nil {
-			Error.Printf("Invalid pid: ", data[1])
+			Error.Printf("Invalid PID: ", data[1])
 		}
 
-		if state == "start" && length == 3 {
+		if state == "register" && length == 3 {
 			timeout, err := strconv.ParseInt(data[2], 10, 32)
 			if err != nil {
 				Error.Printf("Invalid timeout: ", data[2])
 			}
 			registerProcess(int(pid), time.Now().Unix(), timeout)
-		} else if state == "done" && length == 2 {
+		} else if state == "unregister" && length == 2 {
 			unregisterProcess(int(pid))
 		} else {
 			Error.Printf("Invalid message: ", raw)
